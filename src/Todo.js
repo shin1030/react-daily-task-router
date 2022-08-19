3d4f7e5265adaa60adf986411e5863f667805e24
Shin <auel1030@gmail.com>
2022-08-19 23:15:09 +0800

    feat(router): Router連結練習2

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,22 @@
+import { useNavigate } from "react-router-dom"
+
 export function Todo() {
-  return <p>這是 Todo 頁面
-  </p>;
+  
+  function LogOut() {
+    const navigate = useNavigate()
+
+    return (
+      <button onClick={() => {
+        navigate('/login')}}>
+        Log Out
+      </button>
+    )
+  }
+  return (
+    <>
+      <LogOut />
+      <p>這是 Todo 頁面</p>
+    </>
+  )
+
 }
